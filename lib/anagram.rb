f81cfac29059dfe44070bb2a86a36b736abb282a
Ashley M Mader <ashley.mader12@gmail.com>
2020-03-15 17:16:25 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,8 +8,8 @@ class Anagram
   end
   
   def match(word_array)
-    word_array.select do |new|
-      new.split("").sort == @word.split("").sort
+    word_array.select do |word|
+      word.split("").sort == @word.split("").sort
     end
   end
 
